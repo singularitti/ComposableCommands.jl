07243baaed5f6e69f53da1d97eab8296486a330a
singularitti <singularitti@outlook.com>
2023-06-27 14:31:31 -0400

    Add docs in src/CommandComposer.jl

--- src/CommandComposer.jl
+++ src/CommandComposer.jl
@@ -5,6 +5,16 @@ export Flag,
 
 abstract type CommandParameter end
 
+"""
+    Flag(long_name, short_name, description)
+
+Create a `Flag` object.
+
+# Arguments
+- `long_name::String`: the full name of the flag, e.g. "recursive" for a `-r` flag.
+- `short_name::String`: the short, usually single-letter, name of the flag, e.g. "r" for a `--recursive` flag.
+- `description::String`: a description of the flag for documentation purposes.
+"""
 struct Flag <: CommandParameter
     long_name::String
     short_name::String
@@ -21,6 +31,17 @@ struct Flag <: CommandParameter
     end
 end
 
+"""
+    Option(long_name, short_name, description, value)
+
+Create an `Option` object.
+
+# Arguments
+- `long_name::String`: the full name of the option, e.g. "file" for a `-f` option.
+- `short_name::String`: the short, usually single-letter, name of the option, e.g. "f" for a `--file` option.
+- `description::String`: a description of the option for documentation purposes.
+- `value::Any`: the value assigned to this option.
+"""
 struct Option <: CommandParameter
     long_name::String
     short_name::String
@@ -40,21 +61,45 @@ end
 
 abstract type AbstractCommand end
 
+"""
+    AndCommands(a::AbstractCommand, b::AbstractCommand)
+
+Represents a conjunction of two commands (i.e., both commands are executed).
+"""
 struct AndCommands <: AbstractCommand
     a::AbstractCommand
     b::AbstractCommand
 end
 
+"""
+    OrCommands(a::AbstractCommand, b::AbstractCommand)
+
+Represents a disjunction of two commands (i.e., either command is executed).
+"""
 struct OrCommands <: AbstractCommand
     a::AbstractCommand
     b::AbstractCommand
 end
 
+"""
+    CommandPipe(a::AbstractCommand, b::AbstractCommand)
+
+Represents a pipeline of two commands (i.e., the output of the first command is used as input to the second command).
+"""
 struct CommandPipe <: AbstractCommand
     a::AbstractCommand
     b::AbstractCommand
 end
 
+"""
+    Redirect(operator::String, target::String)
+
+Represents a redirection of command output or input.
+
+# Arguments
+- `operator::String`: the operator for the redirection, can be one of ("<", "<<", ">", ">>", "2>", "&>", ">&", "2>&1").
+- `target::String`: the target file for the redirection.
+"""
 struct Redirect
     operator::String
     target::String
@@ -66,11 +111,28 @@ struct Redirect
     end
 end
 
+"""
+    CommandRedirect(command::AbstractCommand, redirect::Redirect)
+
+Represents a command with an associated redirection.
+"""
 struct CommandRedirect <: AbstractCommand
     command::AbstractCommand
     redirect::Redirect
 end
 
+"""
+    Command(name, flags, options, arguments, subcommands)
+
+Represents a command to be executed, with associated flags, options, arguments, and subcommands.
+
+# Arguments
+- `name::String`: the name of the command.
+- `flags::Vector{Flag}`: any flags associated with the command.
+- `options::Vector{Option}`: any options associated with the command.
+- `arguments::Vector{String}`: any arguments to the command.
+- `subcommands::Vector{AbstractCommand}`: any subcommands to be executed in conjunction with the main command.
+"""
 struct Command <: AbstractCommand
     name::String
     flags::Vector{Flag}
