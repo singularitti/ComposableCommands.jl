a0957a9d7d4cc9a318f4d256fe17aeabae0e69c1
singularitti <singularitti@outlook.com>
2023-06-27 15:09:24 -0400

    Add more tests in test/interpret.jl

--- test/interpret.jl
+++ test/interpret.jl
@@ -11,6 +11,7 @@
         global git = Command("git", [], [], [], [remote])
         cmd = interpret(git)
         @test cmd == `git remote --verbose show -n origin`
+        @test typeof(cmd) == Cmd
     end
     @testset "Test `rm` command with flags" begin
         recursive = Flag(
@@ -22,6 +23,7 @@
         global rm_redirect = CommandRedirect(rm, Redirect(">", "logfile"))
         cmd = interpret(rm_redirect)
         @test cmd == pipeline(`rm --recursive --force '*.txt'`, "logfile")
+        @test typeof(cmd) == Base.CmdRedirect
     end
     @testset "Test `ls` command with flags" begin
         l = Flag("long-format", "l", "use a long listing format")
@@ -30,28 +32,38 @@
         global ls = Command("ls", [l, a, d], [], [], [])
         cmd = interpret(ls)
         @test cmd == `ls --long-format --all --directory`
+        @test typeof(cmd) == Cmd
     end
     @testset "Test `CommandRedirect` for `ls`" begin
         ls_out = CommandRedirect(ls, Redirect(">", "out.txt"))
         cmd = interpret(ls_out)
         @test cmd == pipeline(`ls --long-format --all --directory`, "out.txt")
+        @test typeof(cmd) == Base.CmdRedirect
     end
     @testset "Test `CommandRedirect` with both input and output" begin
         ls_in = CommandRedirect(ls, Redirect("<", "in.txt"))
-        @test interpret(ls_in) == pipeline("in.txt", `ls --long-format --all --directory`)
+        cmd = interpret(ls_in)
+        @test cmd == pipeline("in.txt", `ls --long-format --all --directory`)
+        @test typeof(cmd) == Base.CmdRedirect
         ls_in_out = CommandRedirect(ls_in, Redirect(">", "out.txt"))
         cmd = interpret(ls_in_out)
+        @test cmd == pipeline(
+            `ls --long-format --all --directory`; stdin="in.txt", stdout="out.txt"
+        )
+        @test typeof(cmd) == Base.CmdRedirect
     end
     @testset "Test `grep` command with an option and value" begin
         aft = Option("after-context", "A", "print NUM lines of trailing context", 3)
         grep = Command("grep", [], [aft], ["pattern", "file.txt"], [])
         cmd = interpret(grep)
         @test cmd == `grep --after-context=3 pattern file.txt`
+        @test typeof(cmd) == Cmd
         @testset "Test `grep` command with a short option and value" begin
             aft = Option("", "A", "print NUM lines of trailing context", 3)
             grep = Command("grep", [], [aft], ["pattern", "file.txt"], [])
             cmd = interpret(grep)
             @test cmd == `grep -A 3 pattern file.txt`
+            @test typeof(cmd) == Cmd
         end
     end
     @testset "Test `AndCommands`" begin
@@ -60,11 +72,13 @@
         @test cmd ==
             `git remote --verbose show -n origin` &
               pipeline(`rm --recursive --force '*.txt'`, "logfile")
+        @test typeof(cmd) == Base.AndCmds
     end
     @testset "Test `OrCommands`" begin
         or = OrCommands(git, ls)
         cmd = interpret(or)
         @test cmd == pipeline(interpret(git), interpret(ls))
+        @test typeof(cmd) == Base.OrCmds
     end
     @testset "Test `CommandPipe`" begin
         l = Flag("long-format", "l", "use a long listing format")
@@ -73,5 +87,6 @@
         pipe = CommandPipe(ls, grep)
         cmd = interpret(pipe)
         @test cmd == pipeline(`ls --long-format`, `grep .bashrc`)
+        @test typeof(cmd) == Base.OrCmds
     end
 end
