72c599a48cab82a8de07fdfb0fcc9d8277e37ff0
singularitti <singularitti@outlook.com>
2026-03-13 00:24:30 -0500

    Reformat src/show.jl

--- src/show.jl
+++ src/show.jl
@@ -1,6 +1,4 @@
 import Base: show
 import AbstractTrees: print_tree
 
-# simple show method that utilizes AbstractTrees to draw a real tree
 show(io::IO, command::AbstractCommand) = print_tree(io, command)
-
